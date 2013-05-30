195f5c1e94f0d4d2961445022b10e8fdf13c28fc
Sean Redmond <github-smr@sneakemail.com>
2013-05-30 16:49:22 -0400

    First commit

---
+++ src/NYPL/BiblioCommons/Api/Client.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * @file
+ * Client class 
+ */
+
+namespace NYPL\BiblioCommons\Api;
+
+class Client {
+
+  const API_BASE = 'https://api.bibliocommons.com';
+  const API_VERSION = 'v1';
+
+  protected $apikey = NULL;
+  protected $conn = NULL;
+
+  /**
+   * Client object constructor.
+   *
+   * @param string $key 
+   *   Your API key
+   */
+  public function __construct($key, $conn = NULL) {
+    $this->apikey = $key;
+    if ($conn === NULL) {
+      $this->conn = new \HTTP_Request2();
+    }
+    else {
+      $this->conn = $conn;
+    }
+  }
+
+  public function apikey() {
+    return $this->apikey;
+  }
+
+  public function conn() {
+    return $this->conn;
+  }
+
+  public function getEndpoint($path, $params=array()) {
+    $all_params = array_merge($params, array('api_key' => 'few2vjhmdnhjhw7xnajj9wxt'));
+    $url = implode('/', array(
+      self::API_BASE,
+      self::API_VERSION,
+      $path));
+    $this->conn->setUrl($url);
+    $this->conn->getUrl()->setQueryVariables($all_params);
+    $data =  $this->conn->send()->getBody();
+    return json_decode($data);
+
+  }
+}
---
+++ tests/client_tests.php
@@ -0,0 +1,71 @@
+<?php
+
+require 'vendor/autoload.php';
+
+
+
+class ClientTest extends PHPUnit_Framework_TestCase
+{
+  const NYPL_RESP = '
+  {
+      "library": {
+          "catalog_url": "http://nypl.bibliocommons.com",
+          "id": "nypl",
+          "name": "New York Public Library"
+      }
+  }';
+
+
+  public function testMinimalConstructor() {
+    $client = new NYPL\BiblioCommons\Api\Client('abcdef');
+
+    # It saved the key we passed
+    $this->assertEquals('abcdef', $client->apikey());
+
+    # created it own HTTP client
+    $this->assertInstanceOf('HTTP_Request2', $client->conn());
+  }
+
+  public function testOptionalConstructor() {
+    $conn = new \HTTP_Request2();
+
+    $client = new NYPL\BiblioCommons\Api\Client('abcdef', $conn);
+
+    # It saved the key we passed
+    $this->assertEquals('abcdef', $client->apikey());
+
+    # It saved the HTTP client we passed
+    $this->assertSame($conn, $client->conn());
+  }
+
+  public function testGettingEndpoint() {
+    $conn_stub = $this->getMock('HTTP_Request2');
+    $response_stub = $this->getMockBuilder('HTTP_Request2_Response')
+      ->disableOriginalConstructor()
+      ->getMock();
+    $url_stub = $this->getMockBuilder('Net_URL2')
+      ->disableOriginalConstructor()
+      ->getMock();
+
+
+    $conn_stub->expects($this->any())
+      ->method('setUrl')
+      ->with($this->equalTo('https://api.bibliocommons.com/v1/libraries/nypl'));
+    $conn_stub->expects($this->any())
+      ->method('send')
+      ->will($this->returnValue($response_stub));
+    $conn_stub->expects($this->any())
+      ->method('getUrl')
+      ->will($this->returnValue($url_stub));
+    $response_stub->expects($this->any())
+      ->method('getBody')
+      ->will($this->returnValue(self::NYPL_RESP));
+
+    $client = new NYPL\BiblioCommons\Api\Client('abcdef', $conn_stub);
+
+    $this->assertEquals(
+      'New York Public Library', 
+      $client->getEndpoint('libraries/nypl')->library->name);
+  }
+}
+
