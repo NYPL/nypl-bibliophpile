cc9a2440419ccedcb7677d1b1e0a06f5bc6d1484
Sean Redmond <github-smr@sneakemail.com>
2013-05-31 17:33:33 -0400

    Fix up to meet Drupal standard

--- src/NYPL/BiblioCommons/Api/Client.php
+++ src/NYPL/BiblioCommons/Api/Client.php
@@ -31,15 +31,39 @@ class Client {
     }
   }
 
+  /**
+   * Fetch the api key supplied to the Client.
+   *
+   * @return string
+   *   The key
+   */
   public function apikey() {
     return $this->apikey;
   }
 
+  /**
+   * Fetch HTTP Client this Client this client is using.
+   *
+   * @return HTTP_Request2
+   *   The HTTP client
+   */
   public function conn() {
     return $this->conn;
   }
 
-  public function getEndpoint($path, $params=array()) {
+  /**
+   * Get an endpoint and parse the JSON.
+   *
+   * @param string $path
+   *   The path fragment for the endpoint that is, the part of the URL 
+   *   following the version string. This should not begin with a slash
+   * @param array $params
+   *   Extra options to be passed the endpoint as query parameters
+   *
+   * @return StdObj
+   *   The parsed JSON
+   */
+  public function getEndpoint($path, $params = array()) {
     $all_params = array_merge($params, array('api_key' => 'few2vjhmdnhjhw7xnajj9wxt'));
     $url = implode('/', array(
       self::API_BASE,
@@ -47,11 +71,20 @@ class Client {
       $path));
     $this->conn->setUrl($url);
     $this->conn->getUrl()->setQueryVariables($all_params);
-    $data =  $this->conn->send()->getBody();
+    $data = $this->conn->send()->getBody();
     return json_decode($data);
 
   }
 
+  /**
+   * Get a Library by ID.
+   *
+   * @param string $id
+   *   The library's id (e.g. 'nypl')
+   *
+   * @return Library
+   *   The Library object
+   */
   public function library($id) {
     $path = 'libraries/' . $id;
     return new Library($this->getEndPoint($path)->library, $this);
