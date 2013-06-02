5926e199d36ee0627a6c4f8aebbde7cf4f79aea3
Sean Redmond <github-smr@sneakemail.com>
2013-06-02 13:19:57 -0400

    Add DataResource tests

---
+++ tests/DataResourceTest.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @file
+ * Tests for DataResource class 
+ */
+
+require 'vendor/autoload.php';
+include_once 'response_helpers.inc';
+
+class DataResourceTest extends PHPUnit_Framework_TestCase {
+
+
+  /**
+   * Set up tests.
+   */
+  protected function setUp() {
+
+    // We're going to use Series as an example DataResource.
+    global $_series_response_no_number;
+
+    $this->resource = new NYPL\BiblioCommons\Api\Series(
+      json_decode($_series_response_no_number));
+  }
+
+  /**
+   * Make sure we can we build a resource from JSON.
+   */
+  public function testConstructorWorks() {
+    $this->assertInstanceOf('NYPL\BiblioCommons\Api\Series', $this->resource);
+  }
+
+  /**
+   * Make sure optional property is converted from empty string to null.
+   */
+  public function testInitializationOfEmptyOptionalProperties() {
+    $this->assertNull($this->resource->number());
+  }
+}
