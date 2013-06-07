f9e01b17494b647e639802e3c8612f6af837b057
Sean Redmond <github-smr@sneakemail.com>
2013-06-07 16:50:19 -0400

    Change tabs to spaces

--- src/NYPL/Bibliophpile/Users.php
+++ src/NYPL/Bibliophpile/Users.php
@@ -12,14 +12,14 @@ namespace NYPL\Bibliophpile;
  */
 class Users extends PaginatedResource {
   protected function initItems() {
-  	$users = array();
-  	foreach ($this->data->users as $user) {
-  		$users[] = new User($user, $this->client);
-  	}
-  	$this->data->users = $users;
+    $users = array();
+    foreach ($this->data->users as $user) {
+        $users[] = new User($user, $this->client);
+    }
+    $this->data->users = $users;
   }
 
   public function users() {
-  	return $this->data->users;
+    return $this->data->users;
   }
 }
